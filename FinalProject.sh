29a1d7618a64d270b98238b469927f9f6449460d
audreane20 <81461020+audreane20@users.noreply.github.com>
2024-11-14 19:59:35 -0500

    Update FinalProject.sh

--- FinalProject.sh
+++ FinalProject.sh
@@ -2,86 +2,85 @@
 
 echo "XYX Corp LTD."
 Main_Menu_Function () {
-echo "M A I N - M E N U"
-PS3="Enter your choice [1-7] "
+while true; do 
+echo ""
+echo -e "\e[1m\e[36m\e[4mM A I N - M E N U\e[0m"
+PS3=$'\nEnter an option [1-7]: ' 
 options=("System Status" "Backup" "Network" "Services" "User Management" "File Management" "Exit")
 select option in "${options[@]}"
 do
 	case $option in 
 		"System Status")
 			System_Status
-			PS3="Enter your choice [1-7] "
-			echo "M A I N - M E N U"
-			echo "1) System Status"
-			echo "2) Backup"
-			echo "3) Network"
-			echo "4) Services"
-			echo "5) User Management"
-			echo "6) File Management"
-			echo "7) Exit"
-
+			break
 		;;
 		"Backup")
 			Backup
-			PS3="Enter your choice [1-7] "
-			echo "M A I N - M E N U"
-			echo "1) System Status"
-			echo "2) Backup"
-			echo "3) Network"
-			echo "4) Services"
-			echo "5) User Management"
-			echo "6) File Management"
-			echo "7) Exit"
-		;;
+			break
+   		;;
 		"Network")
 			Network
+   			break
 		;;
 		"Services")
 			Services
+   			break
 		;;
 		"User Management")
 			User_Management
+   			break
 		;;
 		"File Management")
 			File_Management
+   			break
 		;;
 		"Exit")
-			echo "Exiting..."
-			break
+			echo "Exiting Program..."
+			exit 0
 		;;
 		*)
 			echo "Error: Wrong input"
 		;;
 	esac
+ done
 done
 }
 
 #System Status
 System_Status() {
-echo "S Y S T E M - S T A T U S"
-PS3="Enter an option [1-5] "
-ssMenu=("Memory Status" "CPU Temperature" "Active Processes" "Stop and Close Process" "Main Menu")
+echo ""
+echo -e "\e[1m\e[34m\e[4mSYSTEM - STATUS\e[0m"
+PS3=$'\nEnter an option [1-6]: '
+ssMenu=("Memory Status" "CPU Temperature" "Active Processes" "Stop and Close Process" "Main Menu" "Exit Program")
 select option in "${ssMenu[@]}"
 do
 	case $option in
 		"Memory Status")
 			echo "Memory Status"
+   			break
 		;;
 		"CPU Temperature")
 			echo "CPU Temperature"
+   			break
 		;;
 		"Active Processes")
 			echo "Active Processes"
+   			break
 		;;
 		"Stop and Close Process")
 			echo "Stop and Close Process"
+   			break
 		;;
 		"Main Menu")
 			echo "Going Back to Main Menu..."
-			echo ""
+                        echo ""
+                        PS3=$'\nEnter your choice [1-7]: '
 			break
-			#Error Here if you go to back to main menu and exit it doesnt work
 		;;
+ 		"Exit Program")
+                        echo "Exiting Program..."
+                        exit 0
+                ;;
 		*)
 			echo "Error: Wrong input"
 		;;
